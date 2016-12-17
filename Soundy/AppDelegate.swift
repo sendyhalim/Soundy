c834188ecce19948f36d255afe06a711391f2a6e
Sendy Halim <sendyhalim93@gmail.com>
2016-12-17 20:23:40 +0700

    Initial commit

---
+++ Soundy/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Soundy
+//
+//  Created by Sendy Halim on 12/17/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+}
---
+++ Soundy/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Soundy
+//
+//  Created by Sendy Halim on 12/17/16.
+//  Copyright © 2016 Sendy Halim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+      // Update the view, if already loaded.
+    }
+  }
+}
