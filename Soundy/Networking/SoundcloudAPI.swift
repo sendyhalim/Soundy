9da7a8696abd4543c56485b76b231df11f267b1b
Sendy Halim <sendyhalim93@gmail.com>
2017-01-25 00:20:05 +0700

    Add SoundcloudAPI moya target

---
+++ Soundy/Networking/SoundcloudAPI.swift
@@ -0,0 +1,60 @@
+//
+//  SoundcloudAPI.swift
+//  Soundy
+//
+//  Created by Sendy Halim on 1/25/17.
+//  Copyright © 2017 Sendy Halim. All rights reserved.
+//
+
+import Foundation
+import Moya
+import RxMoya
+
+enum SoundcloudAPI {
+  case searchTracks(String)
+}
+
+extension SoundcloudAPI: TargetType {
+  static private let clientId = "327f7a3adac5989141fe01ce0416b1aa"
+
+  var baseURL: URL {
+    return URL(string: "http://api.soundcloud.com")!
+  }
+
+  var path: String {
+    switch self {
+      case .searchTracks(_):
+        return "/tracks"
+    }
+  }
+
+  var method: Moya.Method {
+    return Moya.Method.get
+  }
+
+  var parameters: [String: Any]? {
+    switch self {
+    case .searchTracks(let searchQuery):
+      return [
+        "q": searchQuery,
+        "linked_partitioning": "1",
+        "client_id": SoundcloudAPI.clientId
+      ]
+    }
+  }
+
+  var parameterEncoding: ParameterEncoding {
+    return URLEncoding.default
+  }
+
+  var sampleData: Data {
+    switch self {
+    case .searchTracks(_):
+      return "{\"collection\": [], \"next_href\": null}".data(using: String.Encoding.utf8)!
+    }
+  }
+
+  var task: Task {
+    return Task.request
+  }
+}
