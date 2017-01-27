e0a6f67eb628b27521de68c6b3fd21e7b9818a15
Sendy Halim <sendyhalim93@gmail.com>
2017-01-27 10:17:47 +0700

    Add MusicPlayer service

---
+++ Soundy/Services/MusicPlayer.swift
@@ -0,0 +1,51 @@
+//
+//  MusicPlayer.swift
+//  Soundy
+//
+//  Created by Sendy Halim on 1/27/17.
+//  Copyright © 2017 Sendy Halim. All rights reserved.
+//
+
+import AVFoundation
+import Foundation
+
+protocol MusicPlayer {
+  var volume: Float { get set }
+  func play(track: Track)
+  func pause()
+  func replay()
+}
+
+struct AVMusicPlayer {
+  let player: AVPlayer = AVPlayer()
+}
+extension AVMusicPlayer: MusicPlayer {
+  static private let zeroTime = kCMTimeZero
+
+  var volume: Float {
+    get {
+      return player.volume
+    }
+
+    set {
+      player.volume = volume
+    }
+  }
+
+  func play(track: Track) {
+    let currentItem = AVPlayerItem(url: track.streamURL)
+
+    player.replaceCurrentItem(with: currentItem)
+    player.play()
+  }
+
+  func pause() {
+    player.pause()
+  }
+
+  func replay() {
+    player.seek(to: AVMusicPlayer.zeroTime) { _ in
+      self.player.play()
+    }
+  }
+}
