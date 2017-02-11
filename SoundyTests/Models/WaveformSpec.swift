0c770fbed75a7647c82d9111a3eccc3d7babc205
Sendy Halim <sendyhalim93@gmail.com>
2017-02-12 02:38:08 +0700

    Fix test cases by normalizing bar heights

--- SoundyTests/Models/WaveformSpec.swift
+++ SoundyTests/Models/WaveformSpec.swift
@@ -1,21 +1,27 @@
 import Nimble
 import Quick
+import Swiftz
 @testable import Soundy
 
 class WaveformSpec: QuickSpec {
   override func spec() {
     describe("private.averageBarHeights()") {
       let barHeights = [3.0, 10.0, 2.0, 3.0, 12.0, 7.0, 8.0, 2.0, 6.0]
+      let normalizeExpectedBarHeights: ([Double]) -> [Double] = { heights in
+        heights.map {
+          ($0 * Config.Track.maxBarHeight).rounded()
+        }
+      }
 
       context("when range is 1") {
         it("should return unmodified bar heights") {
-          expect(averageBarHeights(range: 1, barHeights: barHeights)) == barHeights
+          expect(averageBarHeights(range: 1, barHeights: barHeights)) == normalizeExpectedBarHeights(barHeights)
         }
       }
 
       context("when range is 2") {
         it("should not calculate average of the last element") {
-          let expected = [6.5, 2.5, 9.5, 5.0, 6.0]
+          let expected = normalizeExpectedBarHeights([6.5, 2.5, 9.5, 5.0, 6.0])
 
           expect(averageBarHeights(range: 2, barHeights: barHeights)) == expected
         }
@@ -23,7 +29,7 @@ class WaveformSpec: QuickSpec {
 
       context("when range is 3") {
         it("should uniformly calculate the average") {
-          let expected = [5.0, 7.0, 5.0]
+          let expected = normalizeExpectedBarHeights([5.0, 7.33, 5.33])
           let results = averageBarHeights(range: 3, barHeights: barHeights).map {
             $0.rounded()
           }
@@ -38,7 +44,7 @@ class WaveformSpec: QuickSpec {
             $0.rounded()
           }
 
-          expect(results) == [6.0]
+          expect(results) == normalizeExpectedBarHeights([5.89])
         }
       }
     }
